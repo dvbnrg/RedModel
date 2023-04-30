2d0febf556838dff2c29896d2512e15664ad929b
justdave <justdave@linux.com>
2023-04-30 18:36:37 -0500

    added set events test

--- main_test.go
+++ main_test.go
@@ -2,13 +2,49 @@ package main
 
 import (
 	"context"
-	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
 	pb "redModel/pb"
 	"reflect"
 	"testing"
+
+	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func Test_server_GetProduct(t *testing.T) {
+func Test_server_SetEvent(t *testing.T) {
+	type fields struct {
+		UnimplementedEventServiceServer pb.UnimplementedEventServiceServer
+	}
+	type args struct {
+		ctx context.Context
+		in  *pb.SetEventRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *pb.SetEventResponse
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{"Hello Test Event", fields{}, args{context.Background(), &pb.SetEventRequest{E: &pb.Event{Id: "1", Name: "testEvent", Description: "test Event Description", Time: &timestamppb.Timestamp{}}}}, &pb.SetEventResponse{}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &server{
+				UnimplementedEventServiceServer: tt.fields.UnimplementedEventServiceServer,
+			}
+			got, err := s.SetEvent(tt.args.ctx, tt.args.in)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("server.SetEvent() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("server.SetEvent() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_server_GetEvent(t *testing.T) {
 	type fields struct {
 		UnimplementedEventServiceServer pb.UnimplementedEventServiceServer
 	}
